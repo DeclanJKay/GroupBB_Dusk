39131b6101a1380f56d02b0f98de94f3f04d4ea5
Mateusz <mateusz.wosik2002@gmail.com>
2025-12-07 17:29:19 +0000

    got rid of test print

--- Systems.hpp
+++ Systems.hpp
@@ -286,7 +286,6 @@ class EntityManager : public Registry
     
         void HandleEnemySafeMove(Entity ent)
         {
-            std::cout<<has<EnemySafeMove, Velocity, Position>(ent)<<"\n";
             if (!has<EnemySafeMove, WeaponArsenal, Velocity, Position>(ent)){return;}
             if (has<EnemyShootingLogic>(ent))
             {
